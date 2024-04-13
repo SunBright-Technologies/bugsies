73f3c298d3800819c604e63226a731eb8893046b
Roger Clotet <roger@clotet.dev>
2024-04-13 22:24:15 +0200

    Add test return types

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,28 +2,28 @@ from bugsies.main import DEFAULT_FORMAT, create_bugsie, create_versioned_bugsie,
 from bugsies.models import SupportedFormat
 
 
-def test_create_bugsie():
+def test_create_bugsie() -> None:
     res = create_bugsie()
     assert res["message"] == "Placeholder: create bugsie"
     assert res["version"] == "v1"
     assert res["seed"] == ""
 
 
-def test_create_versioned_bugsie():
+def test_create_versioned_bugsie() -> None:
     res = create_versioned_bugsie("v2")
     assert res["message"] == "Placeholder: create bugsie"
     assert res["version"] == "v2"
     assert res["seed"] == ""
 
 
-def test_get_bugsie():
+def test_get_bugsie() -> None:
     res = get_bugsie("any_id")
     assert res["message"] == "Placeholder: get bugsie"
     assert res["id"] == res["id"]
     assert res["format"] == DEFAULT_FORMAT
 
 
-def test_get_bugsie_with_format():
+def test_get_bugsie_with_format() -> None:
     res = get_bugsie_with_format("any_id", SupportedFormat.PNG)
     assert res["message"] == "Placeholder: get bugsie"
     assert res["id"] == res["id"]
